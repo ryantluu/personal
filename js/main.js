593a86e8bc8c5e1de6568b925ba22c8cc5e9cd59
Ryan Luu <ryanluu@Ryans-MBP.home>
2017-01-15 23:32:53 -0500

    Update 2017-01-16T04:32:53.379Z

---
+++ js/main.js
@@ -0,0 +1,1 @@
+function toggleMenu(){document.getElementById("sidebar").classList.toggle("active")}function hideMenu(){document.getElementById("sidebar").classList.remove("active")}document.getElementById("hamburger-menu").onclick=function(){toggleMenu()},document.getElementById("project-list").onclick=function(){hideMenu()},document.getElementById("footer").onclick=function(){hideMenu()};
